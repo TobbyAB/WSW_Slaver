5af45bbb558ac7be71cfe60837b8885b6fa4e918
tobbyab <tobby@sentientech.com.cn>
2023-01-31 15:29:21 +0800

    V0.0.3
    1,更新原V0.0.3

--- applications/Relay.c
+++ applications/Relay.c
@@ -12,42 +12,61 @@
 #include "pin_config.h"
 #include "led.h"
 #include "radio_encoder.h"
-
+#include <stdio.h>
 
 #define DBG_TAG "relay"
 #define DBG_LVL DBG_INFO
 #include <rtdbg.h>
 
+
+uint8_t Relay1_Status = 0, Relay2_Status = 0;
+
 void Relay_Init(void)
 {
-    rt_pin_mode(RELAY1,PIN_MODE_OUTPUT);
-    rt_pin_mode(RELAY2,PIN_MODE_OUTPUT);
-    rt_pin_write(RELAY1,0);
-    rt_pin_write(RELAY2,0);
+    rt_pin_mode(RELAY1, PIN_MODE_OUTPUT);
+    rt_pin_mode(RELAY2, PIN_MODE_OUTPUT);
+    rt_pin_write(RELAY1, 0);
+    rt_pin_write(RELAY2, 0);
     LOG_I("Relay_Init Success\r\n");
 }
 
 void Relay1_Open(void)
 {
-    rt_pin_write(RELAY1,1);
-    LOG_I("Relay1 to Open \r\n");
+    if (Relay1_Status == 0)
+    {
+        Relay1_Status = 1;
+        rt_pin_write(RELAY1, 1);
+        LOG_I("Relay1 to Open \r\n");
+    }
 }
-MSH_CMD_EXPORT(Relay1_Open,Relay1_Open);
+MSH_CMD_EXPORT(Relay1_Open, Relay1_Open);
 
 void Relay1_Close(void)
 {
-    rt_pin_write(RELAY1,0);
-    LOG_I("Relay1 to Close \r\n");
+    if (Relay1_Status == 1)
+    {
+        Relay1_Status = 0;
+        rt_pin_write(RELAY1, 0);
+        LOG_I("Relay1 to Close \r\n");
+    }
 }
-MSH_CMD_EXPORT(Relay1_Close,Relay1_Close);
+MSH_CMD_EXPORT(Relay1_Close, Relay1_Close);
 void Relay2_Open(void)
 {
-    rt_pin_write(RELAY2,1);
-    LOG_I("Relay2 to Open \r\n");
+    if (Relay2_Status == 0)
+    {
+        Relay2_Status = 1;
+        rt_pin_write(RELAY2, 1);
+        LOG_I("Relay2 to Open \r\n");
+    }
 }
 
 void Relay2_Close(void)
 {
-    rt_pin_write(RELAY2,0);
-    LOG_I("Relay2 to Close \r\n");
+    if (Relay2_Status == 0)
+    {
+        Relay2_Status = 1;
+        rt_pin_write(RELAY2, 0);
+        LOG_I("Relay2 to Close \r\n");
+    }
 }
